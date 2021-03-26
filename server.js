4bbea40848ae49c61a97a77c8f36643921cb5f26
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-25 20:20:12 -0400

    updated server.js

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //bring in mongoose.connect
-
+const connection = mongoose.connect;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 //Declare routes
-
+const api = require('./routes/api');
+const html = require('./routes/html');
 //Declare api routes
+app.use('/api', api);
+app.use('/html', html);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
