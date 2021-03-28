cd18269f71fab7c4beef1c9d9aa39393719286ac
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-27 22:29:12 -0400

    saving all recent changes

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 //bring in mongoose.connect
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
