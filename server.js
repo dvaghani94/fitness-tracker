e8ecdfd3107d831660cf3770cd0ac0d70225a922
dvaghani <dhruvi.vaghani@gmail.com>
2021-03-28 12:29:14 -0400

    routes working

--- server.js
+++ server.js
@@ -17,13 +17,16 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useFindAndModify: false,
 });
 
-//Declare routes
-const api = require('./routes/api');
-const html = require('./routes/html');
+// //Declare routes
+// const api = require('./routes/api');
+// const html = require('./routes/html');
 
 //Declare api routes
-app.use('/api', api);
-app.use('/html', html);
+// app.use('/api', api);
+// app.use('/html', html);
+
+app.use(require("./routes/api.js"))
+app.use(require("./routes/html.js"))
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
